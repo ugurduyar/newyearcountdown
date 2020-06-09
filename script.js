b1ce997149d38ece97f6b2488ea0d76b4b82d96a
ugurduyar <ismailugurduyar@gmail.com>
2020-06-09 22:30:38 +0300

    DOM elements added

---
+++ script.js
@@ -0,0 +1,13 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const countdown = document.getElementById("countdown");
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+function updateCountdown() {}
+
+updateCountdown();
